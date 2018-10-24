6362e63320eb54d27a70604f5e70031706405ae0
sebastiantorge <sebysebyy89@gmail.com>
2018-10-24 10:30:33 +0300

    Updated css & js files

--- functions/enqueues.php
+++ functions/enqueues.php
@@ -14,8 +14,8 @@ if ( ! function_exists('b4st_enqueues') ) {
 		wp_register_style('fontawesome5-css', 'https://use.fontawesome.com/releases/v5.4.1/css/all.css', false, '5.4.1', null);
 		wp_enqueue_style('fontawesome5-css');
 
-		wp_register_style('b4st-css', get_template_directory_uri() . '/theme/css/b4st.css', false, null);
-		wp_enqueue_style('b4st-css');
+		wp_register_style('wp-dpc-css', get_template_directory_uri() . '/theme/css/wp-dpc.css', false, null);
+		wp_enqueue_style('wp-dpc-css');
 
 		// Scripts
 
@@ -31,8 +31,8 @@ if ( ! function_exists('b4st_enqueues') ) {
 		wp_register_script('bootstrap-js', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.min.js', false, '4.1.3', true);
 		wp_enqueue_script('bootstrap-js');
 
-		wp_register_script('b4st-js', get_template_directory_uri() . '/theme/js/b4st.js', false, null, true);
-		wp_enqueue_script('b4st-js');
+		wp_register_script('wp-dpc-js', get_template_directory_uri() . '/theme/js/wp-dpc.js', false, null, true);
+		wp_enqueue_script('wp-dpc-js');
 
 		if (is_singular() && comments_open() && get_option('thread_comments')) {
 			wp_enqueue_script('comment-reply');
